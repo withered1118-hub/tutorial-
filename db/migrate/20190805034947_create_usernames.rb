6f889c1a92746c32067a7fb5a8aa5ec98a058743
withered <jazz2001@126.com>
2019-08-05 11:53:58 +0800

    i

---
+++ db/migrate/20190805034947_create_usernames.rb
@@ -0,0 +1,13 @@
+class CreateUsernames < ActiveRecord::Migration[5.2]
+  def change
+    create_table :usernames do |t|
+      t.string :
+      t.string :string
+      t.string :email
+      t.string :
+      t.string :string
+
+      t.timestamps
+    end
+  end
+end
