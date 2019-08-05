3516cc6d29e9125b3a92d73f31433bb3eec54321
withered <jazz2001@126.com>
2019-08-05 13:06:40 +0800

    i

--- db/migrate/20190805034947_create_usernames.rb
+++ db/migrate/20190805034947_create_usernames.rb
@@ -1,10 +1,10 @@
 class CreateUsernames < ActiveRecord::Migration[5.2]
   def change
     create_table :usernames do |t|
-      t.string :
+      t.string 
       t.string :string
       t.string :email
-      t.string :
+      t.string 
       t.string :string
 
       t.timestamps
